4e17ab7524983df90166198fae097ec6ce6afaa8
sinsehwan <sk124590@gmail.com>
2025-10-02 15:10:15 +0900

    feat(task): implement basic task domain #1

--- src/main/java/com/example/backendStudyExample/task/domain/Task.java
+++ src/main/java/com/example/backendStudyExample/task/domain/Task.java
@@ -1,4 +1,46 @@
 package com.example.backendStudyExample.task.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long id;
+
+    @Column(nullable = false)
+    String title;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    String description;
+    @Column(nullable = false)
+    LocalDateTime dueDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    TaskStatus status;
+
+    public Task(
+            String title,
+            String description,
+            LocalDateTime dueDate,
+            TaskStatus status
+    ) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    protected Task() {
+
+    }
 }
---
+++ src/main/java/com/example/backendStudyExample/task/domain/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.example.backendStudyExample.task.domain;
+
+public enum TaskStatus {
+    TODO, PROCEED, COMPLETED
+}
